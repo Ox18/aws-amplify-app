08c28ce4e93b9b6062e08addbb55cd1db55f744b
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-06-29 14:46:19 -0500

    add functionality to deploy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const onClick = () => {
+    const API = "https://40i6urmoze.execute-api.us-east-1.amazonaws.com/dev";
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({ "firstName": "Wilmer", "lastName": "Delgado" });
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch(API, requestOptions)
+      .then(response => response.text())
+      .then(result => alert(JSON.parse(result).body))
+      .catch(error => console.log('error', error));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +39,8 @@ function App() {
         >
           Learn React
         </a>
+        <br />
+        <button onClick={onClick}>Click Me</button>
       </header>
     </div>
   );
